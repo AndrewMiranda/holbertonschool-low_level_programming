73969e82973ef0ae79f73440638664e62a3eed9b
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2020-10-01 23:26:33 -0500

    Temp

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -5,20 +5,5 @@
  */
 void print_number(int n)
 {
-	unsigned int b;
-
-	if (n < 0)
-	{
-		b = -n;
-		_putchar('-');
-	}
-	else
-	{
-		b = n;
-	}
-	if (b / 10)
-	{
-		print_number(b / 10);
-	}
-	_putchar((b % 10) + '0');
+	_putchar(n);
 }
