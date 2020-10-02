070bc00f098a3e6349828ce124e92401b05f6351
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2020-10-01 23:23:18 -0500

    Fix 100

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,35 +1,27 @@
 #include "holberton.h"
 
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number - Entry point
+ * @n: char variable
+ *
  */
+
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int i;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		i = -n;
+		_putchar('-');
 	}
 	else
 	{
-		m = n;
+		i = n;
 	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
+	if (i / 10)
 	{
-		d /= 10;
-		count *= 10;
+		print_number(i / 10);
 	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar('0' + (i % 10));
 }
