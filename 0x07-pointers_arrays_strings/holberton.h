c32e636266c30e15c8d414b566caeb7e207c0829
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2020-10-05 20:31:08 -0500

    Header

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+#endif
