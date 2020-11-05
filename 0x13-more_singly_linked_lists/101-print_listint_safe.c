51400160c58488332f1e62d522fdaebe95a395fe
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2020-11-05 01:30:28 -0500

    Fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,83 +1,30 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints all the elements of a linked list.
- * @head: head pointer of the linked list.
+ * print_listint_safe - prints all the elements of a listint_t list.
+ * @head: pointer to first node of a listint_t list.
  *
- * Return: The number of nodes.
+ * Return: a size_t number representing the amount of nodes in the list.
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *actual, *loopStart;
-	size_t count;
-	char flag;
+	size_t count = 0;
 
-	if (!head)
+	if (head == NULL)
 		return (0);
 
-	count = 0;
-	loopStart = the_tortoise_and_the_hare(head);/*validates if loop exists*/
-	flag = '\0'; /*Flag is down to go across the list at least one time*/
-
-	for (actual = head; actual; actual = actual->next)
+	while (head != NULL)
 	{
-		if (actual == loopStart && flag)
-		{
-			printf("-> [%p] %d\n", (void *)actual, actual->n);
-			return (count);
-		}
-
-		printf("[%p] %d\n", (void *)actual, actual->n);
 		count++;
-		if (actual == loopStart)
-			flag = '1';
-	}
-	return (count);
-}
-
-/**
- * the_tortoise_and_the_hare - to detect a loop in a linked list.
- * @head: head pointer of the linked list.
- *
- * Return: The node where the loop starts or NULL if no loop.
- */
-const listint_t *the_tortoise_and_the_hare(const listint_t *head)
-{
-	const listint_t *tortoise, *hare, *flag, *broken, *actual;
-	int len, i;
-
-	tortoise = head;
-	hare = tortoise->next;
-
-	if (!tortoise || !hare)
-		return (NULL);
-	len = 0;/*number of nodes of the loop*/
-	while (tortoise != hare)
-	{
-		tortoise = tortoise->next;
-		if (hare)
-			flag = hare->next;
-
-		if (!flag || !hare)
-			return (NULL);
-		hare = (hare->next)->next;
-		len++;
-	}
-	/*The next section is to locate the real beginning of the loop*/
-	broken = head;
-	actual = tortoise;
-	while (!(actual->next == broken))
-	{
-		actual = tortoise;
-		for (i = 0; i < len; i++)
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head->next >= head)
 		{
-			if (actual->next == broken)
-				return (broken);
-			actual = actual->next;
+			printf("-> [%p] %d\n", (void *)head->next, (head->next)->n);
+			break;
 		}
-		broken = broken->next;
+		head = head->next;
 	}
-	if (len == 1)
-		return (tortoise->next);
-	return (tortoise);
+
+	return (count);
 }
