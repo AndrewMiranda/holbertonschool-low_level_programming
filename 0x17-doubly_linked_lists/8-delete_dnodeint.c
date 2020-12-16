600b68165d9f03b918ecbf0aaef457d8bbe359a3
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2020-12-16 13:42:02 -0500

    Fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,51 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * sum_dlistint - function that returns the sum of all the data (n) of a
- * dlistint_t linked list.
- * @head: input of a list.
- * Return: the sum of the list
+ * delete_dnodeint_at_index - deletes the node at index
+ * @head: head of linked list
+ * @index:  index of the node that should be deleted
+ * Return: 1 if success and -1 if failed
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp = *head;
-	dlistint_t *p = NULL;
-	dlistint_t *delete = NULL;
-	unsigned int i = 1;
+	dlistint_t *temp;
+	unsigned int count;
 
-
-	while (index > i)
+	if (*head == NULL)
+		return (-1);
+	temp = *head;
+	for (count = 0; temp != NULL && count < index; count++)
+		temp = temp->next;
+	if (temp == NULL)
+		return (-1);
+	if (count == index)
 	{
-		tmp = tmp->next;
-		i++;
+		if ((*head)->next == NULL)
+		{
+			*head = NULL;
+			free(temp);
+			return (1);
+		}
+		if (temp == (*head))
+		{
+			(*head) = (*head)->next;
+			(*head)->prev = NULL;
+			free(temp);
+			return (1);
+		}
+		if (temp->next == NULL)
+		{
+			temp->prev->next = NULL;
+			free(temp);
+			return (1);
+		}
+		temp->next->prev = temp->prev;
+		temp->prev->next = temp->next;
+		free(temp);
+		return (1);
 	}
-	if (tmp == NULL)
-		return(-1);
-	if (tmp == *head)
-	{
-		delete = tmp;
-		tmp = tmp->next;
-		*head = tmp;
-		if (tmp != NULL)
-			tmp->prev = NULL;
-	}
-	else if (tmp->next->next == NULL)
-	{
-		tmp->next->prev = NULL;
-		delete = tmp->next;
-		tmp->next = NULL;
-
-	}
-	else
-	{
-		delete = tmp->next;
-		p = tmp->next->next;
-		tmp->next->next = NULL;
-		tmp->next->prev = NULL;
-		tmp->next = p;
-		p->prev = tmp;
-	}
-	free(delete);
-	return(1);
+	return (-1);
 }
